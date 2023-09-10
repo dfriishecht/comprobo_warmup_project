5842994658a13368fa3dd796376680ccf23d4c1c
dfriishecht <dfriishecht@olin.edu>
2023-09-10 16:54:11 -0400

    Add Class and Function docstring.

--- warmup_project/teleop.py
+++ warmup_project/teleop.py
@@ -30,15 +30,37 @@ class Teleop(Node):
     """
 
     def __init__(self):
+        """
+        Constructs all required attributes for the Teleop Node.
+
+        Parameters:
+            vel_pud - publisher node
+                A node which publishes the 'cmd_vel' topic.
+            timer - a timer node
+                A node which governs the node's loop rate.
+        """
         super().__init__("teleop_node")
         self.vel_pub = self.create_publisher(Twist, "cmd_vel", 10)
         timer_period = 0.2  # seconds
         self.timer = self.create_timer(timer_period, callback=self.run_loop)
 
     def run_loop(self):
+        """
+        Executes the teleop runtime loop.
+
+        Checks for keyboard from the user. Based on the input publishes
+        the corresponding velocity commands.
+        """
         msg = Twist()
 
         def get_key():
+            """
+            Reads keyboard inputs and converts them to a string.
+
+            Returns:
+                key - string
+                    A string representing the current key pressed.
+            """
             tty.setraw(sys.stdin.fileno())
             select.select([sys.stdin], [], [], 0)
             key = sys.stdin.read(1)
@@ -65,6 +87,9 @@ class Teleop(Node):
 
 
 def main():
+    """
+    Initializes and publishes the teleop node.
+    """
     rclpy.init()
 
     teleop_publish = Teleop()
