4ea3fb20c24c0354013a81b82950c0592068fc33
dfriishecht <dfriishecht@olin.edu>
2023-09-17 11:50:48 -0400

    initial obstacle avoider refactor.

--- warmup_project/obstacle_avoider.py
+++ warmup_project/obstacle_avoider.py
@@ -15,37 +15,31 @@ class ObstacleAvoider(Node):
             LaserScan, "scan", callback=self.get_laser, qos_profile=10
         )
 
-        self.vel_pub = self.create_publisher(Twist, "cmd_vel", 10)
-
         self.odom_sub = self.create_subscription(
-            Odom, "odom", callback=self.avoid_obstacle, qos_profile=10
+            Odom, "odom", callback=self.get_angle, qos_profile=10
         )
 
+        self.vel_pub = self.create_publisher(Twist, "cmd_vel", 10)
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, callback=self.run_loop)
-        #
+
+        self.current_angle = None
+        self.dists = {
+            "deg0": 1.5,
+            "deg90": 2.0,
+            "deg270": 2.0,
+        }
+
+        self.obstacle_detect = False
         self.lin_vel = 0.0
         self.ang_vel = 0.0
-        #
-        self.current_angle = None
-        self.prev_coord = [0, 0]
-        self.goal_angle = 90
-        self.lin_goal_reached = False
-        self.done = False
-        self.avoid_turn = False
-        self.begin_turn = False
-        self.begin_obstacle_move = False
-        self.dists = None
-        self.obstacle_range = 1.0
-        self.ang_direction = None
-        self.turn_reset = True
-        self.avoid_move = False
+        self.obstacle_threshold = 1.0
 
-    def get_angle(self, w):
+    def get_angle(self, msg):
         """
         Get current neato angle
         """
-        return rad2deg(acos(w) * 2)
+        self.current_angle = rad2deg(acos(msg.pose.pose.orientation.w) * 2)
 
     def get_laser(self, msg):
         """
@@ -57,92 +51,24 @@ class ObstacleAvoider(Node):
             "deg270": msg.ranges[270],
         }
 
-    def check_obstacle(self):
-
-        if self.dists["deg0"] > self.obstacle_range or self.dists["deg0"] == 0.0:
-            return False
-        self.begin_turn = False
-        return True
-
-    def avoid_obstacle(self, msg):
-        if self.avoid_turn is True:
-            self.turn_reset = False
-            self.begin_turn = False
-            return
-        self.current_angle = self.get_angle(msg.pose.pose.orientation.w)
-        if self.begin_turn is False:
-            self.lin_vel = 0.0
-            if self.dists["deg90"] >= self.dists["deg270"]:
-                self.ang_direction = -1.0
-            else:
-                self.ang_direction = 1.0
-            self.ang_vel = 0.3 * self.ang_direction
-            self.goal_angle = self.current_angle + 90 * self.ang_direction
-            self.begin_turn = True
-            self.avoid_turn = False
-
-        if self.ang_direction == -1.0 and self.avoid_turn is False:
-            if self.current_angle <= self.goal_angle:
-                self.ang_vel = 0.0
-                self.avoid_turn = True
-                self.begin_obstacle_move = False
-        elif self.ang_direction == 1.0 and self.avoid_turn is False:
-            if self.current_angle >= self.goal_angle:
-                self.ang_vel = 0.0
-                self.avoid_turn = True
-                self.begin_obstacle_move = False
-
-    def obstacle_move(self):
-        if self.avoid_move is True:
-            return
-        if self.begin_obstacle_move is False:
+    def check_front_obstacle(self):
+        """
+        Checks for obstacle in front of neato
+        """
+        if self.dists["deg0"] > self.obstacle_threshold or self.dists["deg0"] == 0.0:
+            self.obstacle_detect = False
             self.lin_vel = 0.3
-            self.avoid_move = False
-            self.begin_obstacle_move = True
-        if self.ang_direction == -1.0 and self.avoid_move is False:
-            if self.dists["deg90"] > self.obstacle_range or self.dists["deg90"] == 0.0:
-                self.lin_vel = 0.0
-                self.avoid_move = True
-        if self.ang_direction == 1.0 and self.avoid_move is False:
-            if (
-                self.dists["deg270"] < self.obstacle_range
-                or self.dists["deg270"] == 0.0
-            ):
-                self.lin_vel = 0.0
-                self.avoid_move = True
-
-    def continue_path(self, msg):
-
-        self.current_angle = self.get_angle(msg.pose.pose.orientation.w)
-        if self.begin_turn is False:
-            self.ang_vel = -0.3 * self.ang_direction
-            self.begin_turn = True
-        if self.ang_direction == -1.0:
-            if self.current_angle >= 0.0:
-                self.ang_vel = 0.0
-                self.turn_reset = True
-        elif self.ang_direction == 1.0:
-            if self.current_angle <= 0.0:
-                self.ang_vel = 0.0
-                self.turn_reset = True
+        else:
+            self.obstacle_detect = True
+            self.lin_vel = 0.0
 
     def run_loop(self):
         """
         Executes the Node runtime loop and publishes the "cmd_vel" topic
         """
         msg = Twist()
-        odom = Odom()
-        self.check_obstacle()
-        if self.begin_turn is False and self.turn_reset is True:
-            self.avoid_obstacle(odom)
-        elif self.avoid_turn is True and self.avoid_move is False:
-            self.obstacle_move()
-        elif self.avoid_move is True and self.turn_reset is False:
-            self.continue_path(odom)
-
+        self.check_front_obstacle()
         msg.linear.x = self.lin_vel
-        msg.angular.z = self.ang_vel
-
         self.vel_pub.publish(msg)
 
 
